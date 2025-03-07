c3f7da9b8ccd90dd0bde1720c3cdbe62eaadf93c
ABISHEK <abideepu19.vrs@gmail.com>
2025-03-07 20:44:47 +0530

    Added the Card from UIVerse

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import gamelogo from "../../assets/gameicon2.png";
 import toggle_light from "../../assets/night.png";
@@ -8,8 +8,10 @@ import search_icon_dark from "../../assets/search-b.png";
 import { Link } from "react-router-dom";
 
 function Navbar(props) {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   const toggle_mode = () => {
-    props.theme == "light" ? props.setTheme("dark") : props.setTheme("light");
+    props.theme === "light" ? props.setTheme("dark") : props.setTheme("light");
   };
 
   return (
@@ -18,14 +20,29 @@ function Navbar(props) {
         <img src={gamelogo} alt="Game Logo" className="logo" />
         <p>GameHub</p>
       </div>
-      <ul>
+
+      {/* Hamburger Menu */}
+      <div className="menu-icon" onClick={() => setMenuOpen(!menuOpen)}>
+        ☰
+      </div>
+
+      {/* Navigation Links */}
+      <ul className={`nav-links ${menuOpen ? "active" : ""}`}>
         <li>
-          <Link className="react-link" to="/home">
+          <Link
+            className="react-link"
+            to="/home"
+            onClick={() => setMenuOpen(false)}
+          >
             Home
           </Link>
         </li>
         <li>
-          <Link className="react-link" to="/">
+          <Link
+            className="react-link"
+            to="/"
+            onClick={() => setMenuOpen(false)}
+          >
             Games
           </Link>
         </li>
@@ -34,31 +51,39 @@ function Navbar(props) {
             href="https://www.linkedin.com/in/abishek-sivakumar-a16a18263/"
             className="react-link"
             target="_blank"
+            rel="noopener noreferrer"
+            onClick={() => setMenuOpen(false)}
           >
             About
           </a>
         </li>
         <li>
-          <Link className="react-link" to="/addgame">
+          <Link
+            className="react-link"
+            to="/addgame"
+            onClick={() => setMenuOpen(false)}
+          >
             Add Game
           </Link>
         </li>
       </ul>
+
+      {/* Search Box (Hidden on mobile) */}
       <div className="search-box">
         <input type="text" placeholder="Search Games" />
         <img
-          src={props.theme == "light" ? search_icon_light : search_icon_dark}
+          src={props.theme === "light" ? search_icon_light : search_icon_dark}
           alt="Search Icon"
           className="search"
         />
       </div>
+
+      {/* Theme Toggle */}
       <img
-        src={props.theme == "light" ? toggle_light : toggle_dark}
+        src={props.theme === "light" ? toggle_light : toggle_dark}
         alt="Toggle Mode Icon"
         className="toggle-icon"
-        onClick={() => {
-          toggle_mode();
-        }}
+        onClick={toggle_mode}
       />
     </div>
   );
